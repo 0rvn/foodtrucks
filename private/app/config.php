45ce6d5f4c7816c0e7530d15bd8afdeb433eba2d
Delphine <d-martinet@outlook.fr>
2017-07-21 12:16:06 +0200

    initialisation du projet

---
+++ private/app/config.php
@@ -0,0 +1,35 @@
+<?php
+
+// --------------------
+// CONFIG DU PROGRAMME
+// --------------------
+
+// Adresse du serveur de base de données
+// "Localhost" est un alias de 127.0.0.1
+$host = "127.0.0.1";
+
+// Nom d'utilisateur de la base de données
+$user = "root";
+
+// Mot de passe associé à l'utilisateur (root) de la base de données
+$pass = "";
+
+// Nom de la base de données sur laquelle on va travailler
+$database = "foodtrucks";
+
+
+// --------------------
+// CONSTANTES
+// --------------------
+
+// -- Execution mode
+define("MODE", "dev"); // dev | prod
+
+// -- Directories (signifie "répertoire")
+define("VIEWS_DIRECTORY", "../private/views/");
+
+// --------------------
+// DEFAULT VARS
+// --------------------
+
+$default_page = "home";
